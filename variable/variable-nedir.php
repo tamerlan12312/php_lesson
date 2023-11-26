65d1789d605108b0a687231198a643eb0e89cb49
Tamerlan <daniels-95@inbox.ru>
2023-11-26 11:50:04 +0400

    what is variable

---
+++ variable/variable-nedir.php
@@ -0,0 +1,12 @@
+
+
+<?php 
+
+ $price = 5000 ; // jsdeki kimi variable deyeri atılır $ ile 
+ $price2 = 3000 ; 
+ $price3 = 0.20 ;
+
+echo $price + ($price  * $price3) ;  // nöqtəli vergül mütləq olmalıdır
+echo "<br>"; // br ilə alt sətirə keçir
+echo $price2 + ($price2  * $price3) ;
+?>
