994ad81a94283ca702a7891152bae6fa746ddda6
Tamerlan <daniels-95@inbox.ru>
2023-11-26 13:46:07 +0400

    variable rules

---
+++ variable/variable-adlandirma.php
@@ -0,0 +1,18 @@
+<?php
+
+   $number1 = 10 ;
+   $number2 = 20 ;
+// - // comment seklinde alinir # - tek comment /*  code */ cox setirli comment
+//    $1number = 40 ; variable 1ci herfi reqem olmamalidir reqem sonda olmalidir
+//    $number1 = 30 ; // burada 30 cixacaq kodlar yuxardan asagi oxundugu ucun
+// $test f = 59; variable aralarinda bosluq ola bilmez bosluq yerine _ yazmaq olar
+// ctrl + k + c hemin kodu commente alir ctrl + k + u commentden cixarir 
+// eng elifbasindan istifade etmek lazimdir variable adlandirmasinda
+
+$ad = "Tamerlan" ;   // cut ve tek dirnaq daxilindede string kimi yazmaq olur
+$soyad = 'Ferecullayev' ;
+
+$aD = "Tamerlan2" ; // variable adlari boyuk ve kicik herflere hessasdir
+   echo $ad." ".$soyad ;  //, - ile yanbayan yazdirmaq olur yeniden cagirmadan . - ilede birlesdirmek olur
+
+?>
