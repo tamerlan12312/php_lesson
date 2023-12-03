fe5f89f23c0eb12e379bdb77fa9879d2d35f537b
Tamerlan <daniels-95@inbox.ru>
2023-12-03 14:07:28 +0400

    Const / Define

---
+++ variable/contant.php
@@ -0,0 +1,10 @@
+<?php
+  // Contant-sabitlerde bir deyer saxlamaq olur hemin deyeri birde deyisdirmek olmur
+  define("email","admin@gmail.com") ;  //define ile bir contant teyin eledik emaili echo ile cagirdiqda bide admin@gmail.com verecekdir
+    // email = "test" ; // error verecekdir yeniden teyin etmek mumkun deyildir
+  define("password","123") ; // pasword cagirdiqda 123 gelecekdir
+  const name = "Tamerlan" ; // hemcinin const ilede yaratmaq olar
+  echo email."<br>" ; // boyuk ve kicik herife hessasdir
+  echo password."<br>" ;
+  echo name ;
+?>
