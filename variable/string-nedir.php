4ccd2cc43e9faee321cceebd079eb8ee80b49fd9
Tamerlan <daniels-95@inbox.ru>
2024-04-04 12:30:58 +0400

    string methods

--- variable/string-nedir.php
+++ variable/string-nedir.php
@@ -1,29 +1,28 @@
-<?php
- 
-    $name = "Tamerlan";
-    $last_name = "Farajullayev" ; 
-    $age = 24 ;
-
-
-    $message = "my name's {$name} {$last_name} and {$age} years old." ;  
-    // php kodun daha yaxsi oxumasi ucun buruq morterizeden istifade etmek uygundur
-    // string daxiline deyisgen daxil etmek mumkundur
-    // tek dirnaq daxilinde tek dirnaq yazanda onun oxunmasi ucun ters slash istifade etmek lazimdir - \
-    // tek dirnaq olduqda deyisgeni oxumur cut dirnaq olduqda deyisgenin deyerini goturur
-    // cut dirnaq olduqda ters slasha ehtiyac yoxdur 
-    echo $message."<br>"                ;
-    echo $message[0]."<br>"             ; //<br> - ile alt setire salir  // m
-    // stringin deyerine indeks nomresine gore catmaq olur
-    echo $message[1]."<br>"             ;// y
-
-    echo strlen($message)."<br>"        ; //strlen () ile stringin sayini goturur - 50
-    echo str_word_count($message)."<br>"; // nece soz oldugunu gosterir - 7
-    echo strtolower($message)."<br>"    ; // butun herfleri kicik edir
-    echo strtoupper($message)."<br>"    ; // butun herfleri boyuk edir
-    echo ucfirst($message)."<br>"       ;  // bas herfi boyuk yazir
-    echo ucwords($message)."<br>"       ; // her sozun bas herfi boyuk yazir
-    echo str_replace(["Tamerlan",24],["Tamerlan 2",25],$message)."<br>" ; //sozu deyisir ilk hansi sozun deyisilmesi digeri yazidigimiz ve hansi variable oldugunu bildiririk
-     //eger birden cox sozun deyisilmesini ucun massiv seklinde yazilir
+<?php 
+     echo "<br>" ;
+     $name = "Tamerlan";
+     $last_name = "Farajullayev";
+     $age = 25;
+     
+     $mesaj = "my name's {$name} last name {$last_name} and {$age} years old";  #eger tek dirnaq daxilinde yene tek dirnaq varsa \ - ters slash istifade olunur
+     #cut dirnaqda normal qaydada olur
+    //  $mesaj1 = "My name's"." ".$name." "."last name"." ".$last_name." "."and"." ".$age." "."years old"; #noqte ile birlesdirmek olar bu sekilde
+    
+    echo $mesaj[0]."<br>" ; # M herfi gelecek
+    echo $mesaj[3]."<br>" ; # n nerfi gelecek 
+    echo strlen($mesaj) ."<br>" ; # herf sayini gosterir - 58
+    echo str_word_count($mesaj)."<br>" ; # soz sayini gosterir
+    echo strtolower($mesaj)."<br>" ; # butun sozu kicik herfe cevirir
+    echo strtoupper($mesaj)."<br>" ; # boyuk herfe cevirir
+    echo ucfirst($mesaj)."<br>" ; # upper case first - ilk herfi boyuk yazir
+    echo str_replace("Tamerlan","Tamer",$mesaj)."<br>" ; # 3 arquemtn qebul edir deyisdireceyimiz soz yazacagimiz soz ve hansi dəyişgende olacaq
+    echo str_replace(["Tamerlan",25],["Tamer",24],$mesaj)."<br>"; # daha cox cox sozden ise massivden istifade olunur
+    echo addcslashes($mesaj,"T")."<br>"; # 2 arqument alir 2ci arqumentde qabagina ters slash qoyur
+    $pass = "Tamerlan" ;
+    echo md5($pass)."<br>" ; # sözü şifrələyir - md5_file text faylini sifreleyir
+    echo sha1($pass)."<br>" ; # sözü şifrələyir  sha1_file text faylini sifreleyir
+    
+    
 
 
 ?>
