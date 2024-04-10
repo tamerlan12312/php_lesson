2c2d5a4a8aa35a05826eb9011c848a0800f02274
Tamerlan <daniels-95@inbox.ru>
2024-04-10 16:35:03 +0400

    function

---
+++ function/function.php
@@ -0,0 +1,10 @@
+<?php
+
+ # bu tip funksiyalar Built funksiyalar adlanir yeni php dili ile hazir gelen funksiyalar
+
+echo date("Y/m/d") ;  # il,ay,gun
+
+echo strlen("Hello world") ;
+
+
+?>
---
+++ function/function_tanimlama.php
@@ -0,0 +1,17 @@
+<?php
+
+function say_hello(){
+    for ($i=0; $i < 10 ; $i++) {  # hemcinin belede
+     echo "Helo world"."<br>" ;
+    }
+}
+say_hello() ;
+
+// say_hello() ;
+
+
+// for ($i=0; $i < 10 ; $i++) {  # bu terzde cagirmaq olur
+//     say_hello() ;
+// }
+
+?>
